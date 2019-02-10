fa98fcfeea9c6563b2507e1ce755242c2fe6cd24
David Buezas <david.buezas@klarna.com>
2019-02-10 18:13:04 +0100

    baender

---
+++ precache-manifest.1b53ee1cb0da9931b9ac51d05547f33f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "af3dde606aac30ab11f7020df330524c",
+    "url": "/static/media/logo.af3dde60.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f784a69156bb90fa7261",
+    "url": "/static/js/main.f784a691.chunk.js"
+  },
+  {
+    "revision": "14a886b205069cef8ace",
+    "url": "/static/js/1.14a886b2.chunk.js"
+  },
+  {
+    "revision": "f784a69156bb90fa7261",
+    "url": "/static/css/main.3e09194b.chunk.css"
+  },
+  {
+    "revision": "14a886b205069cef8ace",
+    "url": "/static/css/1.4a2441ab.chunk.css"
+  },
+  {
+    "revision": "81a76e97b27ffbd056e814a46bd2f30e",
+    "url": "/index.html"
+  }
+];
